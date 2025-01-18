b9d4367ad2acb39989afacca68f4aa6cbedeefcd
erdem-emiroglu <erdem.emirogluu@gmail.com>
2025-01-18 16:40:51 +0300

    fix

--- src/components/buttons/copy-button.tsx
+++ src/components/buttons/copy-button.tsx
@@ -5,20 +5,27 @@ import {Button} from "@mui/material";
 import * as React from "react";
 import {copyTextFormat} from "@/utils/content-utils";
 import {Contents} from "@/components/tables/content-table/columns";
+import {useToast} from "@/providers/toast.provider";
 
 type CopyButtonProps = {
     contents: Contents[]
 }
 
 export default function CopyButton({contents}: CopyButtonProps) {
-    const handleCopy = (data: Contents[]) => {
+    const {openToast} = useToast()
+    const handleCopy = async (data: Contents[]) => {
+
         const newText = copyTextFormat(data);
-        navigator.clipboard.writeText(newText);
+        await navigator.clipboard.writeText(newText);
+        openToast({
+            text: "İçerik kopyalandı",
+            variant: "success"
+        });
     }
     const isDisabled = contents.length === 0;
     return (
         <Button disabled={isDisabled} sx={{ "& .MuiButton-startIcon": { marginRight: "4px" }}} color="inherit" startIcon={<CopyIcon fill="white" height={16} width={16}/>} onClick={() => handleCopy(contents)}>
-            Kayıt ekle
+            Kopyala
         </Button>
     );
 }
